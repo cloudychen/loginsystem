cbdac124b0898263f56e2a146eec528774197c79
cloudychen <cloudychenlei@gmail.com>
2013-07-18 17:47:20 +0800

    修改了一个错误

--- main.php
+++ main.php
@@ -1,5 +1,10 @@
 <?php
-require 'checkSession.php';
+session_start();
+require_once 'checkSession.php';
+require_once 'database.php';
+$user_name = strip_tags($_SESSION["user_name"]);
+$result = $db->Query(" SELECT * FROM user WHERE username='$user_name'");
+print_r($result);
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,7 +13,7 @@ require 'checkSession.php';
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body>
-        <div id="header">用户名：|<a href="/updateuserinfo.php">修改个人信息</a> | <a href="/logout.php">退出</a></div>
+        <div id="header">昵称：<?php echo $result['nickname']." " ?>|<a href="/updateuserinfo.php"> 修改个人信息 </a> | <a href="/logout.php"> 退出 </a></div>
         <div>
             <h2>登陆成功！</h2>
             <h1>:)</h1>
