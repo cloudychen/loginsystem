5f2b94709e470b74a65e7cedc597ae5109367614
cloudychen <cloudychenlei@gmail.com>
2013-07-18 17:46:46 +0800

    登录做了一点修改

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ require_once 'database.php';
 if (!is_null($_POST['username']) && !is_null($_POST['userpwd'])) {
     $user_name = strip_tags($_POST['username']);
     $salt = $db->Query("SELECT salt FROM user WHERE username='$user_name'");
-    $user_pass = md5(md5(strip_tags($_POST['userpwd']).$salt));
+    $user_pass = md5(md5(strip_tags($_POST['userpwd'])).$salt);
     // 在数据库中对输入的用户名和密码进行匹配，成功 $nums 不为0；否则 $nums 为0，登录失败
     $result = $db->Query("SELECT id FROM user WHERE username='$user_name' and password='$user_pass'");
     $nums = $db->Num_rows($result);
