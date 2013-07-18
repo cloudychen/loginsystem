9fcf7324f75f2be58b850715f437e4f8a1bae871
cloudychen <cloudychenlei@gmail.com>
2013-07-18 16:12:36 +0800

    新增服务端检测用户名是否已存在功能

---
+++ checkUsername.php
@@ -0,0 +1,11 @@
+<?php
+    require_once 'database.php';
+  	 $user_name=strip_tags($_POST["username"]);
+  	 $result = $db->Query(" SELECT id FROM user WHERE username='$user_name'");
+  	 $nums = $db->Num_rows($result);
+  	 if ($nums==0) {
+  	 	echo "can";
+  	 }else {
+  	 	echo "cannot";
+  	 }
+?>
