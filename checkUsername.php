646e721b9a751c20f2954dfc4a6d0fb9ad896cee
cloudychen <cloudychenlei@gmail.com>
2013-07-19 11:56:56 +0800

    优化代码格式

--- checkUsername.php
+++ checkUsername.php
@@ -1,11 +1,12 @@
 <?php
-    require_once 'database.php';
-  	 $user_name=strip_tags($_POST["username"]);
-  	 $result = $db->Query(" SELECT id FROM user WHERE username='$user_name'");
-  	 $nums = $db->Num_rows($result);
-  	 if ($nums==0) {
-  	 	echo "can";
-  	 }else {
-  	 	echo "cannot";
-  	 }
+
+require_once 'database.php';
+$user_name = strip_tags($_POST["username"]);
+$result = $db->Query(" SELECT id FROM user WHERE username='$user_name'");
+$nums = $db->Num_rows($result);
+if ($nums == 0) {
+    echo "can";
+} else {
+    echo "cannot";
+}
 ?>
