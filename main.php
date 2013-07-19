a4b8e60c2afcd1336a24ba3091ae57b0c27a40ce
cloudychen <cloudychenlei@gmail.com>
2013-07-19 11:54:18 +0800

    修改数据库使用错误，调整变量名，优化代码格式

--- main.php
+++ main.php
@@ -3,8 +3,8 @@ session_start();
 require_once 'checkSession.php';
 require_once 'database.php';
 $user_name = strip_tags($_SESSION["user_name"]);
-$result = $db->Query(" SELECT * FROM user WHERE username='$user_name'");
-print_r($result);
+$result = $db->Query(' SELECT * FROM user WHERE username="' . $user_name . '"');
+$info = $db->Fetch_array($result);
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,7 +13,7 @@ print_r($result);
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body>
-        <div id="header">昵称：<?php echo $result['nickname']." " ?>|<a href="/updateuserinfo.php"> 修改个人信息 </a> | <a href="/logout.php"> 退出 </a></div>
+        <div id="header">昵称：<?php echo $info['nickname'] . " " ?>|<a href="/updateuserinfo.php"> 修改个人信息 </a> | <a href="/logout.php"> 退出 </a></div>
         <div>
             <h2>登陆成功！</h2>
             <h1>:)</h1>
