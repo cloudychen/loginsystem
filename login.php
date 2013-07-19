a457fe0073044ad9a196530d37e39e70631a2889
cloudychen <cloudychenlei@gmail.com>
2013-07-19 11:55:20 +0800

    修改数据库使用错误，调整变量，优化代码格式

--- login.php
+++ login.php
@@ -5,8 +5,10 @@ session_start();
 require_once 'database.php';
 if (!is_null($_POST['username']) && !is_null($_POST['userpwd'])) {
     $user_name = strip_tags($_POST['username']);
-    $salt = $db->Query("SELECT salt FROM user WHERE username='$user_name'");
-    $user_pass = md5(md5(strip_tags($_POST['userpwd'])).$salt);
+    $result = $db->Query('SELECT salt FROM user WHERE username="' . $user_name . '"');
+    $salts = $db->Fetch_array($result);
+    $salt = $salts['salt'];
+    $user_pass = md5(md5(strip_tags($_POST['userpwd'])) . $salt);
     // 在数据库中对输入的用户名和密码进行匹配，成功 $nums 不为0；否则 $nums 为0，登录失败
     $result = $db->Query("SELECT id FROM user WHERE username='$user_name' and password='$user_pass'");
     $nums = $db->Num_rows($result);
@@ -20,8 +22,7 @@ if (!is_null($_POST['username']) && !is_null($_POST['userpwd'])) {
         $_SESSION['user_name'] = $user_name;
         header("Location: /main.php");
     }
-}
-else{
+} else {
     header("Location: /login.html");
 }
 ?>
