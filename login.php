c8a80b4889d97c63f0231d9b7a477f9dd65cf23c
cloudychen <cloudychenlei@gmail.com>
2013-07-24 11:51:29 +0800

    对变量进行了转义处理

--- login.php
+++ login.php
@@ -4,11 +4,11 @@
 session_start();
 require_once 'database.php';
 if (!is_null($_POST['username']) && !is_null($_POST['userpwd'])) {
-    $user_name = strip_tags($_POST['username']);
+    $user_name = addslashes(strip_tags($_POST['username']));
     $result = $db->Query('SELECT salt FROM user WHERE username="' . $user_name . '"');
     $salts = $db->Fetch_array($result);
     $salt = $salts['salt'];
-    $user_pass = md5(md5(strip_tags($_POST['userpwd'])) . $salt);
+    $user_pass = md5(md5(addslashes(strip_tags($_POST['userpwd']))) . $salt);
     // 在数据库中对输入的用户名和密码进行匹配，成功 $nums 不为0；否则 $nums 为0，登录失败
     $result = $db->Query("SELECT id FROM user WHERE username='$user_name' and password='$user_pass'");
     $nums = $db->Num_rows($result);
