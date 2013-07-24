fe4e6f2f5f5ad0fc6158e18429a03d181d5754a8
cloudychen <cloudychenlei@gmail.com>
2013-07-24 11:51:18 +0800

    对变量进行了转义处理

--- checkUsername.php
+++ checkUsername.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'database.php';
-$user_name = strip_tags($_POST["username"]);
+$user_name = addslashes(strip_tags($_POST["username"]));
 $result = $db->Query(" SELECT id FROM user WHERE username='$user_name'");
 $nums = $db->Num_rows($result);
 if ($nums == 0) {
